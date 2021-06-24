112c560cce988b65756a10e5c38123ebab9f32c9
lbertoncello <lucas_b_andrade@hotmail.com>
2021-06-24 12:33:02 -0300

    corrigido update de usuario

--- src/controllers/v1/UserController/update.js
+++ src/controllers/v1/UserController/update.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
+const stringUtils = require('@utils/string');
 
 async function update (req, res, next) {
 	try {
 		let user = await User.findById(req.user._id);
+		const userData = req.body;
 
-		Object.assign(user, req.body);
+		delete userData.databasesAllowed;
+
+		Object.assign(user, userData);
 		user = await user.save();
 
 		if (user) {
